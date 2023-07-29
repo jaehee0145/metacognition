080abe5ac2452b2c98849c28cb13eeeaa055bd1c
jaehee <jaeheepark0145@gmail.com>
2023-07-29 14:33:57 +0900

    initial commit

---
+++ src/main/java/com/example/metacognition/MetacognitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.metacognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetacognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetacognitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/metacognition/MetacognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metacognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetacognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
